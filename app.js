7658b74fcb0b53d9fa2703bf1b1d655fc363981b
Kevin Castillo Morales <kcastillo@ld.co.cr>
2021-07-19 17:21:23 -0600

    Primer Publicacion

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+// Motor de plantilla
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+
+
+//middleware
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+  res.render("index", { titulo: "Titulo Dinámico Ejs" });
+});
+
+app.get("/servicios", (req, res) => {
+    res.render("servicios", { tituloServicios: "Titulo Dinámico desde Servicios" });
+  });
+
+app.use((req, res, next) => {
+   // res.status(404).send("Sorry cant find that!");
+   res.status(404).render("404",{
+     titulo: "404",
+     descripcion: "Página 404"
+   });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
