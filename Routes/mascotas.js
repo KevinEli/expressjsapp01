2976545b2d294e0d9fc15e802808d5d61067e3fd
EnglishEasy2021* <kevindermo@hotmail.com>
2021-08-02 15:55:39 -0600

    Se versiona sección de eliminar

--- Routes/mascotas.js
+++ Routes/mascotas.js
@@ -14,4 +14,55 @@ router.get("/", async (req, res) => {
 });
 
 
+router.get('/crear', (req, res) => {
+    res.render('crear')
+});
+
+
+router.post('/', async (req, res) => {
+    const body = req.body;
+    try{
+        await Mascota.create(body);
+        res.redirect('/mascotas');
+
+    }catch(error){
+        console.log(error);
+    }
+});
+
+router.get('/:id', async (req, res) => {
+    const id = req.params.id;
+
+    try{
+        const mascotaDB = await Mascota.findOne({ _id: id });
+        res.render('detalle', { mascota: mascotaDB, error: false });
+
+    }catch(error){
+        res.render('detalle', { error: true, mensaje: 'No se encuentra el Id seleccionado' });
+    }
+});
+
+
+router.delete('/:id', async (req, res) => {
+    const id = req.params.id;
+
+    try{
+        const mascotaDB = await Mascota.findByIdAndDelete({ _id: id });
+        if (mascotaDB){
+          res.json({
+              estado:true,
+              mensaje:'Eliminado'
+          });
+        }else{
+          res.json({
+              estado:false,
+              mensaje:'Fallo eliminar'
+          });
+        }
+
+    }catch(error){
+       console.log(error);
+    }
+});
+
 module.exports = router;
--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const app = express();
+
+app.use(express.urlencoded({extended: true})); 
+app.use(express.json());
+
 require('dotenv').config();
 
 
